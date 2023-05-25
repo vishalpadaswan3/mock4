6f71f76863d4091fb2f0ded0d388154c4a6430ac
vishalpadaswan3 <vishalpadaswan1.com>
2023-05-25 15:18:17 +0530

    done part

--- routes/doctor.js
+++ routes/doctor.js
@@ -22,11 +22,48 @@ routerDoctor.get('/appointments', auth, async (req, res) => {
     try {
         
         let em = req.user.email;
-        const doctors = await modelDoctor.find({email:em});
+        const doctors = await modelDoctor.find();
         res.status(200).json({message: 'Doctors fetched successfully', doctors});
     } catch (error) {
         res.status(500).json({message: 'Internal server error', error});
     }
 });
+
+routerDoctor.get('/all/appointments', async (req, res) => {
+    try {
+        
+        const doctors = await modelDoctor.find();
+        console.log(doctors)
+        res.status(200).json({message: 'Doctors all data', doctors});
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error', error});
+    }
+});
+
+
+
+routerDoctor.delete('/appointments/delete/:id', auth, async (req, res) => {
+    try {
+        const id = req.params.id;
+        await modelDoctor.findByIdAndDelete(id);
+        res.status(200).json({message: 'Doctor deleted successfully'});
+
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error', error});
+    }
+});
+
+
+routerDoctor.patch('/appointments/edit/:id', auth, async (req, res) => {
+    try {
+        let id = req.params.id;
+        const user = await modelDoctor.findByIdAndUpdate({_id:id}, req.body);
+        res.status(200).json({message: 'Doctor updated successfully'});
+
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error', error});
+    }
+});
+
 
 module.exports = {routerDoctor};
